153b63deb18314d7c0a2b98a589b32dcdabeb892
Cristian B <cris.bahena77@gmail.com>
2023-12-09 09:48:11 -0600

    added breakfast items

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+breakfast = Breakfast.create(name: "Waffle", description: "Fluffier style pancake with ridges", price: 10.99)
+
+breakfast = Breakfast.create(name: "Pancake", description: "Batter shaped into disc and cooked", price: 9.99)
+
+breakfast = Breakfast.create(name: "Sausage", description: "links made of meat", price: 3.99)
+
+breakfast = Breakfast.create(name: "Burrito", description: "bacon, egg, cheese wrapped in a tortilla", price: 8.99)
+
+breakfast = Breakfast.create(name: "French Toast", description: "bread toasted with egg and sweetened", price: 12.99)
